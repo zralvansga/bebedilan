2ffac5db3201d6dbcd79a1d972dc91056a5b6e29
alvansga <zralvansga97@gmail.com>
2020-09-20 22:33:54 +0700

    Langkah 3
    Signed-off-by: alvansga <zralvansga97@gmail.com>

--- main.py
+++ main.py
@@ -8,11 +8,19 @@ pygame.display.set_caption('Bebedilan')
 
 running = True
 
-playerpos = [100,100]
+sumbuY,sumbuX = 0,1
+playerpos = [100,100] 
 
 player = pygame.image.load("resources/images/dude.png")
 rumput = pygame.image.load("resources/images/grass.png")
 kastil = pygame.image.load("resources/images/castle.png")
+
+keys = {
+    "atas": False,
+    "bawah": False,
+    "kiri": False,
+    "kanan": False
+}
 
 while(running):
     screen.fill(0)
@@ -36,3 +44,32 @@ while(running):
         if event.type == pygame.QUIT:
             pygame.quit()
             exit(0)
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == K_w:
+                keys['atas'] = True
+            elif event.key == K_a:
+                keys['kiri'] = True
+            elif event.key == K_s:
+                keys['bawah'] = True
+            elif event.key == K_d:
+                keys['kanan'] = True
+            
+        if event.type == pygame.KEYUP:
+            if event.key == K_w:
+                keys['atas'] = False
+            elif event.key == K_a:
+                keys['kiri'] = False
+            elif event.key == K_s:
+                keys['bawah'] = False
+            elif event.key == K_d:
+                keys['kanan'] = False
+
+        if keys["atas"]:
+            playerpos[sumbuX] -= 10
+        elif keys["bawah"]:
+            playerpos[sumbuX] += 10
+        elif keys["kiri"]:
+            playerpos[sumbuY] -= 10
+        elif keys["kanan"]:
+            playerpos[sumbuY] += 10
