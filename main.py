dc8a29824e403b0f8772004849e2c008c661e7b1
alvansga <zralvansga97@gmail.com>
2020-09-20 22:20:27 +0700

    Langkah 2
    Signed-off-by: alvansga <zralvansga97@gmail.com>

--- main.py
+++ main.py
@@ -2,19 +2,35 @@ import pygame
 from pygame.locals import *
 
 pygame.init()
-lebar,tinggi = 640, 480
-screen = pygame.display.set_mode((lebar,tinggi))
+width,height = 640, 480
+screen = pygame.display.set_mode((width,height))
+pygame.display.set_caption('Bebedilan')
 
 running = True
 
 playerpos = [100,100]
 
 player = pygame.image.load("resources/images/dude.png")
+rumput = pygame.image.load("resources/images/grass.png")
+kastil = pygame.image.load("resources/images/castle.png")
 
 while(running):
     screen.fill(0)
+
+    for x in range(int(width/rumput.get_width()+1)):
+        for y in range(int(height/rumput.get_height()+1)):
+            screen.blit(rumput, (x*100,y*100))
+
+    screen.blit(kastil,(0,30))
+    screen.blit(kastil,(0,135))
+    screen.blit(kastil,(0,240))
+    screen.blit(kastil,(0,345))
+    
     screen.blit(player,playerpos)
+
     pygame.display.flip()
+    # pygame.display.update() 
+    
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
