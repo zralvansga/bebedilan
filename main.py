7b872922be0bb5eb4a8209591c9e743026affd55
alvansga <zralvansga97@gmail.com>
2020-09-21 11:50:34 +0700

    Langkah 5
    Signed-off-by: alvansga <zralvansga97@gmail.com>

--- main.py
+++ main.py
@@ -9,11 +9,14 @@ pygame.display.set_caption('Bebedilan')
 running = True
 
 sumbuY,sumbuX = 0,1
-playerpos = [100,100] 
+playerpos = [100,100]
+score = 0
+arrow_list = []
 
 player = pygame.image.load("resources/images/dude.png")
 rumput = pygame.image.load("resources/images/grass.png")
 kastil = pygame.image.load("resources/images/castle.png")
+arrow = pygame.image.load("resources/images/bullet.png")
 
 keys = {
     "atas": False,
@@ -35,11 +38,24 @@ while(running):
     screen.blit(kastil,(0,345))
     
     mousepos = pygame.mouse.get_pos()
-    angle = math.atan2(mousepos[1] - (playerpos[1] + 32), mousepos[0] - (playerpos[0] + 26))
+    angle = math.atan2(mousepos[1] - (playerpos[1] + 32), mousepos[0] - (playerpos[0] + 26)) # dalam radian
     playerrot = pygame.transform.rotate(player, 360 - angle * 57.29)
     new_playerpos = (playerpos[0] - playerrot.get_rect().width / 2, playerpos[1] - playerrot.get_rect().height / 2)
 
     screen.blit(playerrot,new_playerpos)
+
+    for bullet in arrow_list:
+        arrow_idx = 0
+        velx = math.cos(bullet[0])*10
+        vely = math.sin(bullet[0])*10
+        bullet[1]+=velx
+        bullet[2]+=vely
+        if bullet[1] < -64 or bullet[1] > width or bullet[2] <-64 or bullet[2] > height:
+            arrow_list.pop(arrow_idx)
+        arrow_idx += 1
+        for projectile in arrow_list:
+            new_arrow = pygame.transform.rotate(arrow, 360-projectile[0] * 57.29)
+            screen.blit(new_arrow, (projectile[1], projectile[2]))
 
     pygame.display.flip()
     # pygame.display.update() 
@@ -49,6 +65,9 @@ while(running):
         if event.type == pygame.QUIT:
             pygame.quit()
             exit(0)
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            arrow_list.append([angle, new_playerpos[0] + 32, new_playerpos[1] + 32])
 
         if event.type == pygame.KEYDOWN:
             if event.key == K_w:
